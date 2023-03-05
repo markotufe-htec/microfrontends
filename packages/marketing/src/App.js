b9fe7b7a24c18f40abfca8d40199cbebe47b9319
markotufe-htec <marko.tufegdzic@htecgroup.com>
2023-03-05 14:01:17 +0100

    Changed marketing prefix5

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import {
-  StylesProvider,
-  createGenerateClassName
-} from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
